d1825c1e49e92a729f465bf0945d5fc3e96dab22
Brian <riceb53@gmail.com>
2019-06-24 18:37:38 -0500

    show random fortune

--- app/controllers/api/fortunes_controller.rb
+++ app/controllers/api/fortunes_controller.rb
@@ -1,5 +1,10 @@
 class Api::FortunesController < ApplicationController
   def the_fortune
+    # make a bunch of fortunes
+    fortunes = ["you will be happy", "you already know the future since you have a rewind button", "you will make the world stand still"]
+    # pick a random one
+    @fortune = fortunes.sample
+    # show the user that thing
     render 'my_fortune.json.jb'
   end
 end
