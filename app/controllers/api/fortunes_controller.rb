a409a645c2687d7a1b584f68d6554fb2340be438
Brian <riceb53@gmail.com>
2019-06-24 18:51:22 -0500

    dry up lotto numbers

--- app/controllers/api/fortunes_controller.rb
+++ app/controllers/api/fortunes_controller.rb
@@ -10,20 +10,12 @@ class Api::FortunesController < ApplicationController
 
   def lottery_numbers
     @winning_numbers = []
-    # make random number from 1-60
-    lottery_number = rand(1..60)
-    # add that number to an array
-    @winning_numbers << lottery_number
-    lottery_number = rand(1..60)
-    @winning_numbers << lottery_number
-    lottery_number = rand(1..60)
-    @winning_numbers << lottery_number
-    lottery_number = rand(1..60)
-    @winning_numbers << lottery_number
-    lottery_number = rand(1..60)
-    @winning_numbers << lottery_number
-    lottery_number = rand(1..60)
-    @winning_numbers << lottery_number
+    6.times do
+      # make random number from 1-60
+      lottery_number = rand(1..60)
+      # add that number to an array
+      @winning_numbers << lottery_number
+    end
     # show that array to the user
     render 'my_lotto.json.jb'
   end
